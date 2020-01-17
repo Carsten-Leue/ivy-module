867c8fa2637614067c71f57a9fe90bf028170cb8
Dr. Carsten Leue <carsten.leue@gmx.net>
2020-01-17 15:12:46 +0100

    initial checkin

---
+++ src/components/sample.component.module.ts
@@ -0,0 +1,12 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { SampleComponent } from './sample.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [SampleComponent],
+  exports: [SampleComponent]
+})
+export class SampleModule {}
